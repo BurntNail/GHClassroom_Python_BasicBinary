9a1da87956f4d373a28b2fe06ab2465fbbcd41a5
Jack Maguire <49815482+BurntNail@users.noreply.github.com>
2024-11-28 07:39:01 +0700

    remove datatypes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def decimal_to_binary (decimal: int) -> [bool]:
+def decimal_to_binary (decimal):
     # fill out this function!
     return []
 
-def binary_addition (a: [bool], b: [bool]) -> [bool]:
+def binary_addition (a, b):
     # also fill out this function!
     return []
